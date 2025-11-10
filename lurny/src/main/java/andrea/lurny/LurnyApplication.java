8e81f27105327a013afdb0fcd4e0a0052bcb654c
andreatripodi98 <andrea.oblio@gmail.com>
2025-11-10 18:49:23 +0100

    ..

---
+++ lurny/src/main/java/andrea/lurny/LurnyApplication.java
@@ -0,0 +1,13 @@
+package andrea.lurny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LurnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LurnyApplication.class, args);
+	}
+
+}
---
+++ lurny/src/test/java/andrea/lurny/LurnyApplicationTests.java
@@ -0,0 +1,13 @@
+package andrea.lurny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LurnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
