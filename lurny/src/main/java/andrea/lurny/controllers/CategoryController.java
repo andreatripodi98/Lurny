0151e093c7a121641ffc255a5bfcfef8a5457219
andreatripodi98 <andrea.oblio@gmail.com>
2025-11-11 04:57:01 +0100

    Controllers

--- lurny/src/main/java/andrea/lurny/controllers/CategoryController.java
+++ lurny/src/main/java/andrea/lurny/controllers/CategoryController.java
@@ -1,4 +1,24 @@
 package andrea.lurny.controllers;
 
+import andrea.lurny.entities.Category;
+import andrea.lurny.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public Page<Category> getAll(@RequestParam(defaultValue = "0") int pageNumber, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "nome") String sortBy) {
+        return categoryRepository.findAll(pageNumber, pageSize, sortBy);
+    }
+
 }
--- lurny/src/main/java/andrea/lurny/repositories/CategoryRepository.java
+++ lurny/src/main/java/andrea/lurny/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package andrea.lurny.repositories;
 
 import andrea.lurny.entities.Category;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ import java.util.UUID;
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
     Optional <Category> findByName(String name);
+
+    Page<Category> findAll(int pageNumber, int pageSize, String sortBy);
 }
